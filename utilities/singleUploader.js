086e4c79becbf1ea41a2e9edad0c2b663ef35522
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-07-09 06:31:35 +0600

    multer fileupload applied

--- utilities/singleUploader.js
+++ utilities/singleUploader.js
@@ -1,31 +1,36 @@
-const multer = require('multer');
+// external imports
+const multer = require("multer");
+const path = require("path");
+const createError = require("http-errors");
 
 function uploader(
-    subfolder_path,
-    allowed_file_types,
-    max_file_size,
-    error_msg
+  subfolder_path,
+  allowed_file_types,
+  max_file_size,
+  error_msg
 ) {
-    //file upload folder
-    const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}/`;
+  // File upload folder
+  const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}/`;
 
-    //define the storage
-    const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, UPLOADS_FOLDER);
-        },
+  // define the storage
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, UPLOADS_FOLDER);
     },
-        filename: (req, file, cb) => {
-            const fileExt = path.extname(file.originalname)
-            const filename = file.originalname
-                .replace(fileExt, "")
-                .toLowerCase()
-                .split("")
-                .join("-") + "-" + Date.now();
-            cb(null, filename + fileExt)
-        })
- return upload;
-}
-    
+    filename: (req, file, cb) => {
+      const fileExt = path.extname(file.originalname);
+      const fileName =
+        file.originalname
+          .replace(fileExt, "")
+          .toLowerCase()
+          .split(" ")
+          .join("-") +
+        "-" +
+        Date.now();
+
+      cb(null, fileName + fileExt);
+    },
+  });
+ 
 
 module.exports = uploader;
