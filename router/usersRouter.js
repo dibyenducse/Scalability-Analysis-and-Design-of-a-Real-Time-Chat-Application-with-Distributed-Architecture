ba63f4a5b09e6c2daa66c6d51540232164170671
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-07-11 02:18:10 +0600

    bug detect on avatarupload

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,10 +3,14 @@ const express = require('express');
 //internal import
 const { getUsers } = require('../controller/usersController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+const avatarUpload = require('../middlewares/common/users/avatarUpload');
 
 const router = express.Router();
 
-//login page
+//Users page
 router.get('/', decorateHtmlResponse('Users'), getUsers);
+
+//add user
+router.post('/', avatarUpload);
 
 module.exports = router;
