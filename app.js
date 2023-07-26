3ff2dfb56cfca2443f0d43cdb524042e52e55bb1
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-07-27 03:20:26 +0600

    update

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connection.on('connected', () => {
 
 //request parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 //set view engine
 app.set('view engine', 'ejs');
