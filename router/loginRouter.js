dfe12ad059a6ebd60068281123290ac49b149084
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-07-31 00:17:29 +0600

    loginController added

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,12 +1,15 @@
 //external import
 const express = require('express');
 //internal import
-const { getLogin } = require('../controller/loginController');
+const { getLogin, login } = require('../controller/loginController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
 
 const router = express.Router();
 
 //login page
 router.get('/', decorateHtmlResponse('Login'), getLogin);
+
+//process login
+router.post('/', login);
 
 module.exports = router;
