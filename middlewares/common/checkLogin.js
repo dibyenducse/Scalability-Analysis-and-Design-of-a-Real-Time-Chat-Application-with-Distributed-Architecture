bc43bd52ad5011c904c13bdab2a4cc1cee505230
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-08-26 00:56:40 +0600

    update authorizarion role

--- middlewares/common/checkLogin.js
+++ middlewares/common/checkLogin.js
@@ -54,13 +54,22 @@ function requireRole(role) {
         if (req.user.role && role.includes(req.user.role)) {
             next();
         } else {
-            res.status(401).json({
-                errors: {
-                    common: {
-                        msg: 'You are not authorized!',
+            if (res.locals.html) {
+                next(
+                    createError(
+                        401,
+                        'You are not authorized to access this page'
+                    )
+                );
+            } else {
+                res.status(401).json({
+                    errors: {
+                        common: {
+                            msg: 'You are not authorized!',
+                        },
                     },
-                },
-            });
+                });
+            }
         }
     };
 }
